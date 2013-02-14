5f301136e21d4f2b6a79545d58cf56d714eb40ce
Nicolas Hafner <shinmera@tymoon.eu>
2013-02-14 15:30:24 +0100

    Implemented sessionfactory.

--- src/org/tymoonnext/bot/module/CmdInterface.java
+++ src/org/tymoonnext/bot/module/CmdInterface.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 import org.tymoonnext.bot.Commons;
 import org.tymoonnext.bot.Kizai;
 import org.tymoonnext.bot.event.CommandEvent;
+import org.tymoonnext.bot.module.auth.SessionFactory;
+import org.tymoonnext.bot.module.auth.User;
 
 /**
  * Primitive Command Line interface
@@ -13,10 +15,12 @@ import org.tymoonnext.bot.event.CommandEvent;
  */
 public class CmdInterface extends ThreadedModule{
     private Scanner in;
+    private SF sf;
     
     public CmdInterface(Kizai bot){
         super(bot);
         in = new Scanner(System.in);
+        sf = new SF(bot);
     }
 
     @Override
@@ -24,13 +28,28 @@ public class CmdInterface extends ThreadedModule{
         while(in==null){try{Thread.sleep(5);}catch(Exception ex){}}
         while(true){
             String command = in.nextLine();
+            sf.authenticate(System.getProperty("user.name"));
             if(command.contains(" "))
-                bot.command(new CommandEvent(Commons.stdout,
-                                             command.substring(0,command.indexOf(' ')),
-                                             command.substring(command.indexOf(' ')+1)));
+                bot.event(new CommandEvent(Commons.stdout,
+                                            command.substring(0,command.indexOf(' ')),
+                                            command.substring(command.indexOf(' ')+1),
+                                            System.getProperty("user.name")));
             else
-                bot.command(new CommandEvent(Commons.stdout,command));
+                bot.event(new CommandEvent(Commons.stdout,
+                                            command,
+                                            null,
+                                            System.getProperty("user.name")));
         }
+    }
+    
+    private class SF extends SessionFactory{
+        public SF(Kizai bot){super(bot);}
+        public void authenticate(String... userinf) {
+            if(getUser(userinf[0]) == null){
+                addUser(new User(userinf[0]));
+            }
+        }
+    
     }
 
 }
