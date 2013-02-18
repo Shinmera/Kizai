b4f090c6a001c79c44c63109ed38fb26f8b40336
shinmera <shinmera@tymoon.eu>
2013-02-18 20:45:32 +0100

    nosave, noload annotations.

---
+++ src/org/tymoonnext/bot/meta/nosave.java
@@ -0,0 +1,16 @@
+package org.tymoonnext.bot.meta;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 
+ * @author Shinmera
+ * @license GPLv3
+ * @version 0.0.0
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface nosave{}
