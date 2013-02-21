d508bcbb0fa06e1882531f3abbd34e88167e61a2
shinmera <shinmera@tymoon.eu>
2013-02-21 22:12:51 +0100

    Added session handling.

--- src/org/tymoonnext/bot/module/auth/User.java
+++ src/org/tymoonnext/bot/module/auth/User.java
@@ -5,7 +5,10 @@ import java.util.logging.Level;
 import org.tymoonnext.bot.Commons;
 
 /**
- *
+ * Auth system user representation. This user class contains an arbitrary
+ * information storage so that multiple AuthImplementors can store their
+ * required data in the user's configuration. If possible, a sub-object should
+ * be used for every AuthImplementor as to avoid collisions.
  * @author Shinmera
  * @license GPLv3
  * @version 0.0.0
@@ -14,6 +17,7 @@ public class User {
     private String name;
     private String UID;
     private DObject config;
+    private boolean loggedIn = false;
     
     public User(DObject conf){
         name = conf.get("name").toString();
@@ -31,7 +35,33 @@ public class User {
         config = conf;
     }
     
-    public void activateSession(){config.set("lastLogin", System.currentTimeMillis());}
+    /**
+     * Sets the last login time to the current time and sets the loggedIn flag
+     * to true.
+     */
+    public void activateSession(){
+        config.set("lastLogin", System.currentTimeMillis());
+        loggedIn = true;
+    }
+    
+    /**
+     * Sets the loggedIn flag to false.
+     */
+    public void deactivateSession(){
+        loggedIn = false;
+    }
+    
+    /**
+     * Returns whether the user is still under an active session. This also
+     * checks the session timeout and actively sets the loggedIn flag in case
+     * the timeout has been reached.
+     * @return Whether the user is still logged in or not.
+     */
+    public boolean isLoggedIn(){
+        long timeSinceLastActivation = System.currentTimeMillis() - getLastLoginTime();
+        if(timeSinceLastActivation > getSessionTimeout()) deactivateSession();
+        return loggedIn;
+    }
     
     public String getName(){return name;}
     public String getUID(){return UID;}
