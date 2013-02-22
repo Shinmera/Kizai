d93ea69816b8f7630e2b26b5ce8e80420ba3928c
Nicolas Hafner <shinmera@tymoon.eu>
2013-02-22 15:36:32 +0100

    Added accessible calls.

--- src/org/tymoonnext/bot/event/EventBind.java
+++ src/org/tymoonnext/bot/event/EventBind.java
@@ -32,7 +32,10 @@ public class EventBind implements Comparable{
      */
     public void invoke(Event ev){
         try{
+            boolean wasAccessible = method.isAccessible();
+            method.setAccessible(true);
             method.invoke(listener, ev);
+            method.setAccessible(wasAccessible);
         }catch(IllegalAccessException ex){
             Commons.log.log(Level.WARNING, toString()+" Failed to invoke method for "+ev, ex);
         }catch(IllegalArgumentException ex){
