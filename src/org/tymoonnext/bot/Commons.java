8e6bb70b97985eca1f9c8d570a547c0a697b03ea
shinmera <shinmera@tymoon.eu>
2013-02-23 17:57:34 +0100

    Minor.

--- src/org/tymoonnext/bot/Commons.java
+++ src/org/tymoonnext/bot/Commons.java
@@ -20,7 +20,7 @@ import org.tymoonnext.bot.stream.Stream;
  * @version 0.0.0
  */
 public class Commons {
-    public static final String VERSION = "2.4.2";
+    public static final String VERSION = "2.5.0";
     public static final String FQDN = "機材";
     public static final String LICENSE = "GPLv3";
     public static final String COREDEV = "TymoonNET/NexT";
