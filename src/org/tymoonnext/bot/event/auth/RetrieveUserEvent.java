c04bb807faa0725c7d9b19c1362876bf19ebdb96
shinmera <shinmera@tymoon.eu>
2013-02-23 14:04:21 +0100

    Added simpler constructor.

--- src/org/tymoonnext/bot/event/auth/RetrieveUserEvent.java
+++ src/org/tymoonnext/bot/event/auth/RetrieveUserEvent.java
@@ -1,5 +1,6 @@
 package org.tymoonnext.bot.event.auth;
 
+import org.tymoonnext.bot.Commons;
 import org.tymoonnext.bot.event.Event;
 import org.tymoonnext.bot.module.auth.User;
 import org.tymoonnext.bot.stream.Stream;
@@ -14,6 +15,7 @@ public class RetrieveUserEvent extends Event{
     private String ident;
     private User user;
     
+    public RetrieveUserEvent(String ident){this(Commons.stdout, ident);}
     public RetrieveUserEvent(Stream origin, String ident){
         super(origin);
         this.ident=ident;
