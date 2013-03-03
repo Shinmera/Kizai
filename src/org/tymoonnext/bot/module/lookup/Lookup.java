e1623184c8c9a52802a7d95d1e8c75dec1ab46f0
Mithent <mithent@gmail.com>
2013-03-03 18:09:36 +0000

    Move Lookup CommandModule import to core.ext namespace

--- src/org/tymoonnext/bot/module/lookup/Lookup.java
+++ src/org/tymoonnext/bot/module/lookup/Lookup.java
@@ -13,7 +13,7 @@ import org.tymoonnext.bot.event.IRCBot.MessageEvent;
 import org.tymoonnext.bot.event.cmd.CommandInstanceEvent;
 import org.tymoonnext.bot.event.core.CommandEvent;
 import org.tymoonnext.bot.module.Module;
-import org.tymoonnext.bot.module.core.CommandModule;
+import org.tymoonnext.bot.module.core.ext.CommandModule;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
@@ -28,33 +28,37 @@ public class Lookup extends Module implements CommandListener, EventListener{
 
     public Lookup(Kizai bot){
         super(bot);
-        CommandModule.register(bot, "lookup", "wikipedia",      "term".split(" "),               "Look up a specified word on Wikipedia.", this);
+        CommandModule.register(bot, "lookup", "wikipedia", "term".split(" "), "Look up a specified word on Wikipedia.", this);
     }
 
     public void shutdown(){
         bot.unbindAllEvents(this);
     }
+    
+    public void onCommand(CommandEvent cmd) {
+        this.invoke("on"+StringUtils.firstToUpper(cmd.getCommand().split(" ")[1]), cmd);
+    }
 
     public void onWikipedia(CommandInstanceEvent evt){
-            String searchTerm = evt.get().getValue("term");
-            try
-            {
-                Document wikipediaResponse = getXML("http://en.wikipedia.org/w/api.php?action=parse&page=" + searchTerm + "&format=xml&prop=text&section=0");
-                
-                Node textNode = wikipediaResponse.getElementsByTagName("text").item(0);
-                
-                String text = textNode.getTextContent();
-                text = text.replace("\n", "").replace("\r", "");
-                
-                String firstParagraph = text.substring(text.indexOf("<p>") + 3, text.indexOf("</p>"));
-                firstParagraph = firstParagraph.replaceAll("<sup.*?>.*?</sup>", "").replaceAll("\\<.*?\\>", "");
-                
-                evt.getStream().send(firstParagraph, evt.getChannel());
-            }
-            catch (Exception e)
-            {
-                evt.getStream().send(e.getMessage(), evt.getChannel());
-            }
+        String searchTerm = evt.get().getValue("term");
+        try
+        {
+            Document wikipediaResponse = getXML("http://en.wikipedia.org/w/api.php?action=parse&page=" + searchTerm + "&format=xml&prop=text&section=0");
+
+            Node textNode = wikipediaResponse.getElementsByTagName("text").item(0);
+
+            String text = textNode.getTextContent();
+            text = text.replace("\n", "").replace("\r", "");
+
+            String firstParagraph = text.substring(text.indexOf("<p>") + 3, text.indexOf("</p>"));
+            firstParagraph = firstParagraph.replaceAll("<sup.*?>.*?</sup>", "").replaceAll("\\<.*?\\>", "");
+
+            evt.getStream().send(firstParagraph, evt.getChannel());
+        }
+        catch (Exception e)
+        {
+            evt.getStream().send(e.getMessage(), evt.getChannel());
+        }
     }
     
     private Document getXML(String uri) throws ParserConfigurationException, SAXException, IOException
@@ -78,9 +82,5 @@ public class Lookup extends Module implements CommandListener, EventListener{
             throw e;
         }
         
-    }
-
-    public void onCommand(CommandEvent cmd) {
-        this.invoke("on"+StringUtils.firstToUpper(cmd.getCommand().split(" ")[1]), cmd);
     }
 }
