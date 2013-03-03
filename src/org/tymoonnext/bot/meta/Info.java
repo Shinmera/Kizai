82149239c608ff8f76f9c9c21c72112ca396965d
shinmera <shinmera@tymoon.eu>
2013-03-03 16:22:47 +0100

    Added Info annotation.

---
+++ src/org/tymoonnext/bot/meta/Info.java
@@ -0,0 +1,19 @@
+package org.tymoonnext.bot.meta;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Description annotation to add more information about a class that can be 
+ * retrieved by the bot at runtime.
+ * @author Shinmera
+ * @license GPLv3
+ * @version 0.0.0
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Info{
+    String txt();
+}
