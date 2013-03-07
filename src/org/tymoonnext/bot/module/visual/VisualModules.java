4ed48ae7c70b73ff5bce0aca5842576ff3b6274c
shinmera <shinmera@tymoon.eu>
2013-03-08 00:32:01 +0100

    Added right click menu.

--- src/org/tymoonnext/bot/module/visual/VisualModules.java
+++ src/org/tymoonnext/bot/module/visual/VisualModules.java
@@ -2,14 +2,23 @@ package org.tymoonnext.bot.module.visual;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Vector;
+import javax.swing.BorderFactory;
 import javax.swing.DefaultListCellRenderer;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
+import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
 import org.tymoonnext.bot.Kizai;
 import org.tymoonnext.bot.event.EventListener;
 import org.tymoonnext.bot.event.core.ModuleLoadEvent;
@@ -23,7 +32,7 @@ import org.tymoonnext.bot.module.Module;
  * @license GPLv3
  * @version 0.0.0
  */
-public class VisualModules extends JPanel implements EventListener{
+public class VisualModules extends JPanel implements EventListener, MouseListener{
     private Kizai bot;
     private Vector<Module> modules;
     private JList list;
@@ -40,6 +49,7 @@ public class VisualModules extends JPanel implements EventListener{
         list = new JList(modules);
         list.setFont(VisualBase.F_MONOSPACED);
         list.setCellRenderer(new ModuleCellRenderer());
+        list.addMouseListener(this);
         
         JScrollPane scroll = new JScrollPane(list);
         scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
@@ -61,6 +71,35 @@ public class VisualModules extends JPanel implements EventListener{
     }
     
     public String toString(){return "[Visual|Modules]";}
+
+    public void mouseClicked(MouseEvent ev){
+        if(ev.getButton() == MouseEvent.BUTTON3){
+            list.setSelectedIndex(list.locationToIndex(ev.getPoint()));
+            Module m = (Module)list.getSelectedValue();
+            
+            ModuleMenuActionListener lst = new ModuleMenuActionListener(bot, m);
+            JPopupMenu menu = new JPopupMenu(m.toString());
+            JMenuItem i_reload = new JMenuItem("Reload Module");
+            JMenuItem i_unload = new JMenuItem("Unload Module");
+            i_reload.addActionListener(lst);
+            i_unload.addActionListener(lst);
+            menu.add(i_reload);
+            menu.add(i_unload);
+            menu.setVisible(true);
+            Border titleUnderline = BorderFactory.createMatteBorder(1, 0, 0, 0, menu.getForeground());
+            TitledBorder labelBorder = BorderFactory.createTitledBorder(titleUnderline, menu.getLabel(),
+                                                                        TitledBorder.CENTER, TitledBorder.ABOVE_TOP, 
+                                                                        menu.getFont(), menu.getForeground());
+            menu.setBorder(BorderFactory.createCompoundBorder(menu.getBorder(),
+            labelBorder));
+            menu.show(list, ev.getX(), ev.getY());
+        }
+    }
+
+    public void mousePressed(MouseEvent e){}
+    public void mouseReleased(MouseEvent e){}
+    public void mouseEntered(MouseEvent e){}
+    public void mouseExited(MouseEvent e){}
     
     class ModuleCellRenderer extends DefaultListCellRenderer {
         public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
@@ -74,4 +113,23 @@ public class VisualModules extends JPanel implements EventListener{
             return label;
         }
     }
+}
+
+class ModuleMenuActionListener implements ActionListener{
+    private Module m;
+    private Kizai bot;
+    
+    public ModuleMenuActionListener(Kizai bot, Module m){
+        this.bot=bot;
+        this.m=m;
+    }
+    
+    public void actionPerformed(ActionEvent e){
+        if(e.getActionCommand().equals("Reload Module")){
+            bot.reloadModule(m);
+        }else if(e.getActionCommand().equals("Unload Module")){
+            bot.unloadModule(m);
+        }
+    }
+    
 }
