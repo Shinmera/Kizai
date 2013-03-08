3bd344f777ca9945376aad5bb0ee815593fc3949
shinmera <shinmera@tymoon.eu>
2013-03-08 17:34:55 +0100

    Added Arguments annotation

---
+++ src/org/tymoonnext/bot/meta/Arguments.java
@@ -0,0 +1,19 @@
+package org.tymoonnext.bot.meta;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Description annotation to add more information about a class that can be 
+ * retrieved by the bot at runtime.
+ * @author Shinmera
+ * @license GPLv3
+ * @version 0.0.0
+ */
+@Target({ElementType.CONSTRUCTOR, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Arguments{
+    String[] value();
+}
