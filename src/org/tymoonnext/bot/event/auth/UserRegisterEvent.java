b453ef4ab1534df3c5a060afe95c4c3d234a3bad
shinmera <shinmera@tymoon.eu>
2013-03-24 09:03:18 +0100

    Added more constructors.

--- src/org/tymoonnext/bot/event/auth/UserRegisterEvent.java
+++ src/org/tymoonnext/bot/event/auth/UserRegisterEvent.java
@@ -1,5 +1,7 @@
 package org.tymoonnext.bot.event.auth;
 
+import NexT.data.DObject;
+import org.tymoonnext.bot.Commons;
 import org.tymoonnext.bot.event.Event;
 import org.tymoonnext.bot.meta.Arguments;
 import org.tymoonnext.bot.module.auth.User;
@@ -13,6 +15,20 @@ import org.tymoonnext.bot.stream.Stream;
  */
 public class UserRegisterEvent extends Event{
     private User user;
+    
+    @Arguments({"username"})
+    public UserRegisterEvent(String name){this(Commons.stdout, name);}
+    
+    @Arguments({"origin", "username"})
+    public UserRegisterEvent(Stream origin, String name){
+        super(origin);
+        DObject conf = new DObject();
+        conf.set("name", name);
+        this.user = new User(conf);
+    }
+    
+    @Arguments({"user"})
+    public UserRegisterEvent(User user){this(Commons.stdout, user);}
     
     @Arguments({"origin", "user"})
     public UserRegisterEvent(Stream origin, User user){
